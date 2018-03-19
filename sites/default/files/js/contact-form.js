b84291972cbc85ff128abd615fdc4f9cfc38b8ab
Ricardo Silva <cornelyus7@gmail.com>
2018-03-19 23:16:54 +0000

    CSS and JS pretty formspree

--- sites/default/files/js/contact-form.js
+++ sites/default/files/js/contact-form.js
@@ -1,20 +1,33 @@
-[].slice.call( document.querySelectorAll( '.input' ) ).forEach( function( inputEl ) {
-    // in case the input is already filled..
-    if( inputEl.value.trim() !== '' ) {
-        classie.add( inputEl.parentNode, 'input--filled' );
+// https://medium.com/@flobin/how-to-set-up-a-contact-form-on-a-static-website-62c2267bacfb
+
+(function() {
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+        (function() {
+            // Make sure we trim BOM and NBSP
+            var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            String.prototype.trim = function() {
+                return this.replace(rtrim, '');
+            };
+        })();
+    }
+    [].slice.call(document.querySelectorAll('.input')).forEach(function(inputEl) {
+        // in case the input is already filled..
+        if (inputEl.value.trim() !== '') {
+            classie.add(inputEl.parentNode, 'input--filled');
+        }
+        // events:
+        inputEl.addEventListener('focus', onInputFocus);
+        inputEl.addEventListener('blur', onInputBlur);
+    });
+
+    function onInputFocus(ev) {
+        classie.add(ev.target.parentNode, 'input--filled');
     }
 
-    // events:
-    inputEl.addEventListener( 'focus', onInputFocus );
-    inputEl.addEventListener( 'blur', onInputBlur );
-} );
-
-function onInputFocus( ev ) {
-      classie.add( ev.target.parentNode, 'input--filled' );
-}
-
-function onInputBlur( ev ) {
-      if( ev.target.value.trim() === '' ) {
-            classie.remove( ev.target.parentNode, 'input--filled' );
-      }
-}
+    function onInputBlur(ev) {
+        if (ev.target.value.trim() === '') {
+            classie.remove(ev.target.parentNode, 'input--filled');
+        }
+    }
+})();
