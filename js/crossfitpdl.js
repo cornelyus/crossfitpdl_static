5ed3cd405bfede6d07131c4885803a85f67913e1
cornelyus7 <cornelyus7@gmail.com>
2019-11-11 23:19:18 +0000

    Back to top button and Sticky nav js.

--- js/crossfitpdl.js
+++ js/crossfitpdl.js
@@ -1,3 +1,4 @@
+//Smooth scroll
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -34,3 +35,69 @@ $('a[href*="#"]')
       }
     }
   });
+
+//Back to top button
+(function(){
+/**
+ * Toggle the visibility of the scroll to top link.
+ */
+  // append  back to top link top body if it is not
+  var exist= jQuery('#back-top').length; // exist = 0 if element doesn't exist
+  if(exist == 0){ // this test is for fixing the ajax bug
+    $("body").append("<p id='back-top'><a href='#top'><span id='button'></span><span id='link'>Back to top</span></a></p>");
+  }
+  // Preview function
+  $("input").change(function () {
+    // building the style for preview
+    var style="<style>#scroll-to-top-prev-container #back-top-prev span#button-prev{ background-color:"+$("#edit-scroll-to-top-bg-color-out").val()+";} #scroll-to-top-prev-container #back-top-prev span#button-prev:hover{ background-color:"+$("#edit-scroll-to-top-bg-color-hover").val()+" }</style>"
+    // building the html content of preview
+    var html="<p id='back-top-prev' style='position:relative;'><a href='#top'><span id='button-prev'></span><span id='link'>";
+    // if label enabled display it
+    if($("#edit-scroll-to-top-display-text").attr('checked')){
+    html+=$("#edit-scroll-to-top-label").val();
+    }
+    html+="</span></a></p>";
+    // update the preview
+    $("#scroll-to-top-prev-container").html(style+html);
+  });
+  $("#back-top").hide();
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+
+    // scroll body to 0px on click
+    $('#back-top a').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+  });
+})();
+
+//Sticky Nav
+(function() {
+      var selector = $('#block-system-main-menu');
+      //only getting the first elmenet in the dom
+      var menu = $(selector).eq(0);
+      if (menu.length) {
+        var breakpoint = menu.offset().top;
+        //we need to compensate the element so that the content does not jump up
+        var compensation = menu.outerHeight();
+        //attaching a scroll event
+        $(window).scroll(function () {
+          if ($(window).scrollTop() > breakpoint) {
+            menu.addClass('stickynav-active');
+            $('body').css('padding-top', compensation);
+          } else {
+            menu.removeClass('stickynav-active');
+            $('body').css('padding-top', 'inherit');
+          }
+        });
+      }
+})();
